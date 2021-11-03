c11aac8c59e72b472139999372d5eaf4b4732979
Ahmed Rashed <ahmed.m.rashed@gmail.com>
2021-11-03 16:04:02 +0200

    More modifications

--- NoisySignal_AutoCorr.m
+++ NoisySignal_AutoCorr.m
@@ -2,51 +2,53 @@ clc
 close all
 clearvars
 
+rng(0); %Generate the same random noise each time this script is executed
+
 A=1; f_0=1;
+T_0=1/f_0;
 
 f_s=200*f_0;
 
-SNR=1/2;  %SNR=-3dB
-f_c=f_s/10;     %cut-off frequency of the digital filter
-[b,a]=butter(9,f_c/(f_s/2));  %designs a 9th-order low-pass digital Butterworth filter (IIR),where b is a vector containing coefficients of a moving average part and a is a vector containing coefficients of an auto-regressive part of the transfer function (see Equation (6.12) of Shin's book).
+SNR=0.5;  %SNR_db=-3
+f_c_by_f_Nyquist=0.5;
 
-T_0=1/f_0;
-T_vec=[2,100,1000]*T_0;
+T_vec=[2,10,100]*T_0;
+N_T=length(T_vec);
+
+T_max=max(T_vec);
+[D_t,K_max]=samplingParameters_T_fs(T_max,f_s);
+t_row=(0:K_max-1)*D_t;
+K_min=floor(min(T_vec)/D_t);
+tau_1_row=(-(K_min-1):K_min-1)*D_t;
+
+x_row=A*sin(2*pi*f_0*t_row);
+x_hat_row=addNoise(x_row,SNR,f_c_by_f_Nyquist);
+
+figure
+ax1=subplot(N_T+1,1,1);
+plot(t_row(1:K_min),[x_hat_row(1:K_min);x_row(1:K_min)])
+xlabel('$t$ (sec.)','interpreter','latex');
+legend({'$\hat{x}(t)=x(t)+n(t)$','$x(t)$'},'interpreter','latex')
+title("$\mathrm{SNR}="+SNR+'$','interpreter','latex')
+
+%Additional optimization of the axes for correct comparison with the correlation curves
+pos=get(ax1,'Position');
+pos(1)=pos(1)+pos(3)/2;
+pos(3)=pos(3)/2;
+set(ax1,'Position',pos,'XGrid','on')
+
+n=1;
 for T=T_vec
-    [D_t,K]=samplingParameters_T_fs(T,f_s);
-    t_vec=(0:K-1)*D_t;
-
-    x_vec=A*sin(2*pi*f_0*t_vec);
-
-    rng(0);
-    n_vec=randn(1,K);   %create broad-band white noise
-    n_vec=filtfilt(b,a,n_vec);  %performs zero-phase digital filtering. (Appendix H of Shin's book) The resulting sequence n is the band-limited (zero to f_c) white noise.
-    n_vec=std(x_vec)/std(n_vec)/sqrt(SNR)*n_vec;
-    x_hat_vec=x_vec+n_vec;
-
-    r_xx_hat=xcorr(x_hat_vec,x_hat_vec,'unbiased');
-    tau=(-(K-1):K-1)*D_t;
-
-    figure
-    subplot(2,1,1)
-    plot(t_vec,x_hat_vec,t_vec,x_vec)
-    xlim([0,2*T_0])
-    xlabel('$t$ (sec.)','interpreter','latex');
-    legend({'$\hat{x}(t)=x(t)+n(t)$','$x(t)$'},'interpreter','latex')
-    title(['$\mathrm{SNR}=',num2str(SNR),',T=',num2str(T),'$ sec'],'interpreter','latex')
-
-    %Additional optimization of the axes for correct comparison with the correlation curves
-    pos=get(gca,'Position');
-    pos(1)=pos(1)+pos(3)/2;
-    pos(3)=pos(3)/2;
-    set(gca,'Position',pos,'XGrid','on')
-
-    subplot(2,1,2)
-    plot(tau,r_xx_hat)
-    xlim(2*T_0*[-1,1])
+    K=floor(T/D_t);
+    r_xx_hat_part=xcorr(x_hat_row(1:K),K_min-1,'unbiased');   %This is part of r_xx_hat. r_xx_hat extends over tau_row=(-(K-1):K-1)*D_t
+    
+    ax=subplot(N_T+1,1,n+1);
+    plot(tau_1_row,r_xx_hat_part)
     xlabel('$\tau$ (sec.)','interpreter','latex');
-    ylabel('$r_{\hat{x}\hat{x}}(\tau)$','interpreter','latex');
-    set(gca,'XGrid','on')
+    ylabel("$r_{\hat{x}\hat{x}}(\tau); \quad T="+T+'$ sec','interpreter','latex');
+    set(ax,'XGrid','on')
+
+    n=n+1;
 end
 
-export_figure(1:length(T_vec),'==',{'s1','s2','s3'})
+export_figure(1,'||',{'s1'})
